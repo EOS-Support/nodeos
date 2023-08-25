4c47084faf02525c86efac7a2b8e124b09178d91
monstredd <92199704+monstredd@users.noreply.github.com>
2023-08-25 10:32:06 +0800

    Update nodeos_p2p_test_tool.py
    read p2p address from file

--- nodeos_p2p_test_tool.py
+++ nodeos_p2p_test_tool.py
@@ -1,24 +1,33 @@
 import socket
+import sys
 
-def test_p2p_address(p2p_address):
-    
-    address, port = p2p_address.split(':')
-    port = int(port)
+def test_p2p_addresses(filename):
+    with open(filename, 'r') as f:
+        p2p_addresses = []
+        
+        for line in f:
+            if not line.startswith('#'):
+                line = line.replace('p2p-peer-address = ', '')
+                p2p_addresses.append(line.rstrip())
+                
+    for p2p_address in p2p_addresses:
+        address, port = p2p_address.split(':')
+        port = int(port)
+        
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            result = sock.connect_ex((address, port))
+            if result == 0:
+                print(f'{p2p_address} is reachable')
+            else:
+                print(f'{p2p_address} is not reachable')
+        except socket.error as e:
+            print(f'Error: {e}')
 
-    try:
-  
-        result = sock.connect_ex((address, port))
-        if result == 0:
-            print(f'{p2p_address} is reachable')
-        else:
-            print(f'{p2p_address} is not reachable')
-    except socket.error as e:
-        print(f'Error: {e}')
+        sock.close()
 
- 
-    sock.close()
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    test_p2p_addresses(filename)
 
-p2p_address = input('Enter P2P address: ')
-test_p2p_address(p2p_address)

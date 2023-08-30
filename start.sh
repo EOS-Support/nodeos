625e6a2cf0594b106f2ebac46aa83c39ea807903
monstredd <92199704+monstredd@users.noreply.github.com>
2023-08-31 02:34:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nodeos --config-dir ./ --data-dir ./eos/data  --disable-replay-opts --trace-no-abis
+nodeos --config-dir ./ --data-dir ./eos/data  --disable-replay-opts --trace-no-abis >>nodeos.log 2>&1 &

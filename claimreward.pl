b7c4dea764b1c5a946a4d3c0c9f2d14386cd3e4a
monstredd <92199704+monstredd@users.noreply.github.com>
2023-09-07 12:35:01 +0800

    Update claimreward.pl

--- claimreward.pl
+++ claimreward.pl
@@ -2,16 +2,21 @@
 
 use strict;
 use warnings;
-my $wallet_pass="/root/pass";  # wallet password file
-my $wallet_name = "test";      # wallet name
+my $wallet_pass="/home/ubuntu/eosio-wallet/support.pass";
+my $wallet_name = "support";
 
 my $unlock_cmd="cat $wallet_pass |cleos wallet unlock -n $wallet_name";
+my $free_powerup_cmd = "curl https://api.eospowerup.io/freePowerup/eossupportbp";
 
 while(1)
 {
 `$unlock_cmd`;
+my $output;
+for (my $i = 0; $i < 2; $i++) {
+`$free_powerup_cmd`;
 my $account = "eossupportbp";
-my $output = `cleos -u https://api.eossupport.io system claimrewards $account`;
+$output = `cleos system claimrewards $account -p $account\@claimer`;
+}
 
 print $output;
 sleep(86400); 
